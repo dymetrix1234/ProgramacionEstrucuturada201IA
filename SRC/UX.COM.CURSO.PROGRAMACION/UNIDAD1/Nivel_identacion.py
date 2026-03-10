2c31b0adc70f0ca0fea73df81c2acfd1e0fa77cf
Rey <dymetrix1234@gmail.com>
2026-03-10 08:03:35 -0600

    EjercicioNIdentacion

---
+++ SRC/UX.COM.CURSO.PROGRAMACION/UNIDAD1/Nivel_identacion.py
@@ -0,0 +1,23 @@
+#ejemplo para visualizar la identacion en python
+
+def explicar_identacion():
+    #nivel 1
+    mensaje = "nivel 1 de identacion"
+    print(mensaje)
+
+    puntos = 10
+
+    if puntos > 9:
+        #nivel 2
+        print ("entra al flujo del if")
+
+        if puntos == 10:
+            #nivel 3
+            print("puntos es igual a 10")
+
+# cierra nivel 1
+def main():
+    explicar_identacion()
+
+if __name__ == "__main__":
+    main()
