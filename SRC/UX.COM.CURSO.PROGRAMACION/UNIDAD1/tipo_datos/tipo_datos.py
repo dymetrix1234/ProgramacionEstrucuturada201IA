c071c50da7e14aa2850ecd027e7fd85b831deea3
Rey <dymetrix1234@gmail.com>
2026-03-10 08:27:38 -0600

    EjercicioTDatos

---
+++ SRC/UX.COM.CURSO.PROGRAMACION/UNIDAD1/tipo_datos/tipo_datos.py
@@ -0,0 +1,18 @@
+def datos():
+
+    # demostracion de tipos de datos en python
+    entero = 25
+    decimal = 3.14
+    cadena = "Hola, mundo!"
+    boleano = True
+
+    print(entero)
+    print(decimal)
+    print(cadena)
+    print(boleano)
+
+def main():
+    datos()
+
+if __name__ == "__main__":
+    main()
