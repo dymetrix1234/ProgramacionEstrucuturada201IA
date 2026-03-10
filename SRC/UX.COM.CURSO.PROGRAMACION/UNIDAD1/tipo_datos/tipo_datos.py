8e303a26a772be9e8153e20c6f9a0a86c8d1fa0e
Rey <dymetrix1234@gmail.com>
2026-03-10 08:32:00 -0600

    EjrcicioTDatos

--- SRC/UX.COM.CURSO.PROGRAMACION/UNIDAD1/tipo_datos/tipo_datos.py
+++ SRC/UX.COM.CURSO.PROGRAMACION/UNIDAD1/tipo_datos/tipo_datos.py
@@ -11,8 +11,18 @@ def datos():
     print(cadena)
     print(boleano)
 
+def tipos_datos_compuesto():
+    lista = [10, 20, 30, 40]
+    tupla = (19, 29, 39, 49)
+    diccionario = {"nombre": "Juan", "edad": 30, "ciudad": "Madrid"}
+
+    print(lista)
+    print(tupla)
+    print(diccionario)
+
 def main():
     datos()
+    tipos_datos_compuesto()
 
 if __name__ == "__main__":
     main()
