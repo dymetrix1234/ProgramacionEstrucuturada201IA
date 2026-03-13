f83c16bb144d9918f301d8d6b0cae28be17c0f7d
Rey <dymetrix1234@gmail.com>
2026-03-13 08:54:11 -0600

    EjercicioDias

--- SRC/UX.COM.CURSO.PROGRAMACION/UNIDAD1/Estructura_Repeticion/Ejercicio_Dias.py
+++ SRC/UX.COM.CURSO.PROGRAMACION/UNIDAD1/Estructura_Repeticion/Ejercicio_Dias.py
@@ -7,6 +7,16 @@ def Dias():
             print("Lunes")
         case "2":
             print("Martes")
+        case "3":
+            print("Miercoles")
+        case "4":
+            print("Jueves")
+        case "5":
+            print("Viernes")
+        case "6":
+            print("Sabado")
+        case "7":
+            print("Domingo")
         case _:
             print ("opcion no valida") 
 
