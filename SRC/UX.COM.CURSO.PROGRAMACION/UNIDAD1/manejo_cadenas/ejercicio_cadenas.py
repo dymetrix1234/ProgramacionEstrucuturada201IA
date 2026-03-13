3585fa03c5c8873e5c904191a35817be27314ae9
Rey <dymetrix1234@gmail.com>
2026-03-13 08:21:49 -0600

    EjercicioCadenas

---
+++ SRC/UX.COM.CURSO.PROGRAMACION/UNIDAD1/manejo_cadenas/ejercicio_cadenas.py
@@ -0,0 +1,14 @@
+#ejercicio cadenas
+def demostracion_cadenas():
+    cadena = "universidad de Xalapa"
+    
+    print(cadena.upper())
+    print(cadena.lower())
+    print(cadena.capitalize())
+    print(cadena.titile())
+
+def main():
+    demostracion_cadenas()
+
+if __name__ == "__main__":
+    main()
