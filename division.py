64c6e701d62c517510324b112b774f02aa021e6f
Rey <dymetrix1234@gmail.com>
2026-05-26 07:49:04 -0600

    Hacker Rank

---
+++ division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # División entera
+    print(a // b)
+    
+    # División de punto flotante
+    print(a / b)
---
+++ operadores.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Suma
+    print(a + b)
+    
+    # Diferencia (resta)
+    print(a - b)
+    
+    # Producto (multiplicación)
+    print(a * b)
+    
+    # División (división)
+    print(a / b)
