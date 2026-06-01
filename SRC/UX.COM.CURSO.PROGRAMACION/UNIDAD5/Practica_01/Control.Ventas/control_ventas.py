249442e12663ce1b5878ec8806971d5c2c8e8bd9
Rey <dymetrix1234@gmail.com>
2026-06-01 07:30:32 -0600

    Fix: Identaciones y main

--- SRC/UX.COM.CURSO.PROGRAMACION/UNIDAD5/Practica_01/Control.Ventas/control_ventas.py
+++ SRC/UX.COM.CURSO.PROGRAMACION/UNIDAD5/Practica_01/Control.Ventas/control_ventas.py
@@ -1,3 +1,4 @@
+# Declaración de estructuras
 def main():
 	productos = ["Laptop", "Smartphone", "Tablet"]
 	dias = ["Lunes", "Martes", "Miércoles"]
@@ -50,3 +51,42 @@ def main():
 if __name__ == "__main__":
 	main()
 
+productos = ["Laptop", "Smartphone", "Tablet"]
+
+ventas = [[0] * 3 for _ in range(3)]
+
+ 
+
+# Lectura de datos
+
+for i in range(3):
+
+    print(f"--- Registro para {productos[i]} ---")
+
+    for j in range(3):
+
+        ventas[i][j] = int(input(f"Ventas del día {j+1}: "))
+
+ 
+
+# Escritura y Reporte
+
+print("\nRESUMEN DE VENTAS")
+
+total_general = 0
+
+ 
+
+for i in range(3):
+
+    suma_producto = sum(ventas[i])
+
+    total_general += suma_producto
+
+    print(f"{productos[i]}: {ventas[i]} | Total: {suma_producto}")
+
+ 
+
+print(f"\nEl total de ventas de la semana es: {total_general}")
+
+print(f"El promedio de ventas es: {total_general / 9:.2f}")
